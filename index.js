d844dfebe520d78fcc9e1695968366a7de046c69
Jonathan Kulak <lackadaize@msn.com>
2024-06-12 09:01:09 -0400

    This is the Initializing Instances Lab.

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your code here
+class Breakfast {
+    constructor(food, drink) {
+        this.food = food
+        this.drink = drink
+    }
+}
+
+const todaysBreakfast = new Breakfast("Eggs", "Orange Juice")
+// console.log(todaysBreakfast)
+
+class Lunch {
+    constructor(salad, soup, drink) {
+        this.salad = salad
+        this.soup = soup
+        this.drink = drink
+    }
+}
+
+const todaysLunch = new Lunch("Ceasar", "Chicken", "Tea")
+// console.log(todaysLunch)
+
+class Dinner {
+    #dessert
+    constructor(salad, soup, entree, dessert) {
+        this.salad = salad
+        this.soup = soup
+        this.entree = entree
+        this.#dessert = dessert
+    }
+
+    get dessert() {
+        return this.#dessert;
+    }
+}
+
+const todaysDinner = new Dinner("Chef", "Beet", "Steak", "Cake")
+// console.log(todaysDinner)
+// console.log(`Let them eat ${todaysDinner.dessert}!`)
